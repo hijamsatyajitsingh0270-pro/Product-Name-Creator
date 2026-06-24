76178d8e8390d878e8e5cb5382d609f122c6572e
Hijam Satyajit Singh <hijamsatyajitsingh0270@gmail.com>
2026-06-24 21:45:59 +0530

    Create product_name_creator.py
    Add product name generator script

---
+++ product_name_creator.py
@@ -0,0 +1,8 @@
+adjectives = ["Smart", "Quick", "Eco", "Nova", "Bright"]
+nouns = ["Tech", "Hub", "Box", "Wave", "Labs"]
+
+print("Generated Product Names:\n")
+
+for adjective in adjectives:
+    for noun in nouns:
+        print(adjective + noun)
